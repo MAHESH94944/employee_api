878e8379ea84c5b7540ef92c62701fcd5abcc388
Mahesh Jadhao <maheshjadhao172@gmail.com>
2025-05-05 11:54:09 +0530

    Enable Vercel Blob storage

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://employee-frontend-nu.vercel.app", //http://localhost:5173
     credentials: true,
   })
 );
